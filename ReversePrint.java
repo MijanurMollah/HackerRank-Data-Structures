1013ed551926937765054fd28c0e498ee3e5ec82
MijanurMollah <44820321+MijanurMollah@users.noreply.github.com>
2019-05-13 11:10:19 -0400

    Create ReversePrint.java

---
+++ ReversePrint.java
@@ -0,0 +1,17 @@
+ // Complete the reversePrint function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void reversePrint(SinglyLinkedListNode head) {
+        if (head.next != null)
+        reversePrint(head.next);
+
+        System.out.println(head.data);
+    }
